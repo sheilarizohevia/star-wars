6ae9ba689e7ba7a08ab2fc08f3031d66a1a0b199
Sheila Rizo Hevia <sheilarizohevia@gmail.com>
2022-03-27 22:32:55 -0400

    add variables in environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
+const urlStarWarsAPI = 'https://swapi.dev/api';
+
 export const environment = {
-  production: true
+  production: true,
+  base: urlStarWarsAPI,
+  films:`${urlStarWarsAPI}/films`,
+  people:`${urlStarWarsAPI}/people`,
+  planets:`${urlStarWarsAPI}/planets`,
+  species:`${urlStarWarsAPI}/species`,
+  starships:`${urlStarWarsAPI}/starships`,
+  vehicles:`${urlStarWarsAPI}/vehicle`
 };
